913aeb8a167e78022a6c9cc8b7eee58520a8e361
nape-brady17 <bcnapier5@yahoo.com>
2024-12-01 11:43:11 -0500

    Initial commit, very basic outline

---
+++ Bacons.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Bacons{
+    public static void main(String[] args) {
+        //call method to read all input and create the graph
+        //call method to randomly select vertex
+        //call method to calculate the average bacon number from the randomly selected vertex
+        //call method to read user selected vertex
+        //call method to calculate the bacon number from this vertex
+        //call method to see if the user wishes to continue
+            //if yes go back to step 4, use a do while loop with a boolean to see if they want to continue
+            //if no continue below to the next step
+        //call method to output a file containing necessary information
+        //call method to thank the user
+    }
+}
